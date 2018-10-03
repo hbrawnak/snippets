4fbbe36450b203be7d30a4964c6d3c1f34826ee5
Habib <hbrawnak@gmail.com>
2018-10-04 00:23:12 +0600

    snippets creations

--- apitut/snippets/views.py
+++ apitut/snippets/views.py
@@ -3,8 +3,25 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from ..snippets.models import Snippet
-from ..snippets.serializers import SnippetSerializer
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+
+    if request.method == 'GET':
+        snippets = Snippet.objects.all
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser.parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.error, status=400)
 
 
 
