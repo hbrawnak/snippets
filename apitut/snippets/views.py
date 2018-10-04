4a61d35892fa052173a39de722c21753f48bafc3
Habib <hbrawnak@gmail.com>
2018-10-04 22:11:38 +0600

    GET PUT DELETE added

--- apitut/snippets/views.py
+++ apitut/snippets/views.py
@@ -23,5 +23,25 @@ def snippet_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.error, status=400)
 
+@csrf_exempt
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
 
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
 
+    elif request.method == 'PUT':
+        data = JSONParser.parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.error, status=400)
+
+    elif request.method == "DELETE":
+        snippet.delete()
+        return JsonResponse(status=204)
